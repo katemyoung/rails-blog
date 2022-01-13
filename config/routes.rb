3a5161c93fa0f3131920ebc9fe112b23232916cc
Kate Young <89363807+katemyoung@users.noreply.github.com>
2022-01-13 21:07:20 +0000

    Add articles index view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'articles/index'
   get "/articles", to: "articles#index"
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
   # get "/articles", to: "articles#index"
 end
