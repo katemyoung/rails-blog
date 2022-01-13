9c53f6c796b9b8f4645ccb7d6cd3fc9363ef7256
Kate Young <89363807+katemyoung@users.noreply.github.com>
2022-01-13 21:34:50 +0000

    Create Article model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get "/articles", to: "articles#index"
   # Defines the root path route ("/")
   root "articles#index"
-  # get "/articles", to: "articles#index"
+
+  get "/articles", to: "articles#index"
 end
