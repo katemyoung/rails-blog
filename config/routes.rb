41896ee91d4d0b12d83693f8418a035b21b519aa
Kate Young <89363807+katemyoung@users.noreply.github.com>
2022-02-04 22:55:57 +0200

    Add resource route helper for articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles 
+  
 end
+
+# These initial routes were replaces with the resources routes method:
+# get "/articles", to: "articles#index"
+# get "/articles/:id", to: "articles#show"
